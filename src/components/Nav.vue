87cae5faacbf0d12ccbbee60dbd700752f4596bf
FFreshman <1366157248@qq.com>
2020-02-21 00:23:23 +0800

    添加了viewport和选中样式改变

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/Money" class="item">
+    <router-link to="/Money" class="item" active-class="selected">
       <Icons name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icons name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icons name="chart"/>
       统计
     </router-link>
@@ -36,6 +36,9 @@
       width: 33.33333333%;
       padding: 2px 0;
       color: #333333;
+    }
+    > .item.selected{
+      color: red;
     }
 
   }
