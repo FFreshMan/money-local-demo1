c48676bd08748c75761c6d04d78e4ca8e165f36e
FFreshman <1366157248@qq.com>
2020-02-20 23:12:09 +0800

    引入svg文件夹和插入页面中

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="nav">
-    <router-link to="/Money">记账</router-link>
-    <router-link to="/labels">标签</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/Money">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>标签
+    </router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#chart"/>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+  //深拷贝，svg后缀可改
   export default {
     name: 'Nav'
   };
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,8 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+declare  module  '*.svg'{
+  const content: string;
+  export  default  content;
+}
+//想要引入svg得先申明一个模块，并将它的string格式导出
