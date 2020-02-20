857bc6f0b7583d011398204135ddba26299dceb0
FFreshman <1366157248@qq.com>
2020-02-20 20:59:28 +0800

    添加404â

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,10 @@ import VueRouter from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 
 Vue.use(VueRouter);
-
 
 const routes = [
   {
@@ -27,6 +27,10 @@ const routes = [
   {
     path: "/Statistics",
     component: Statistics
+  },
+  {
+    path:"*",
+    component: NotFound
   }
   // {
   //   path: '/',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>当前页面不存在</div>
+    <router-link to="/">回到首页</router-link>
+  </div>
+
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
