e832af5f3475d721fc4b896cc4c409ad529e0420
FFreshman <1366157248@qq.com>
2020-03-04 21:16:12 +0800

    完成按天列表展示

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -28,6 +28,7 @@
   import arrType from '@/constants/arrType';
   import arrInterval from '@/constants/arrInterval';
   import dayjs from 'dayjs';
+  import clone from '@/lib/clone';
 
   @Component({
     components: {
@@ -45,33 +46,39 @@
     }
 
     get result() {
-      type Item = RecordItem[];
-      type HashTableItem = { title: string; items: Item }
       const {recordList} = this;
-      const hashTable: { [key: string]: HashTableItem } = {};
-      for (let i = 0; i < recordList.length; i++) {
-        const [date, time] = recordList[i].createdAt!.split('T');
-        hashTable.date = hashTable.date || {title: date, items: []};
-        hashTable.date.items.push(recordList[i]);
+      if (recordList.length === 0) {return [];}
+      const newList = clone(recordList.sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()));
+      const groupList = [{title: newList[0].createdAt, items: [newList[0]]}];
+      for (let i = 1; i < newList.length; i++) {
+        const current = newList[i];
+        const last = groupList[groupList.length - 1];
+        if (dayjs(current.createdAt).isSame(dayjs(last.title), 'day')) {
+          last.items.push(current);
+        } else {
+          groupList.push({title: current.createdAt, items: [current]});
+        }
       }
-      return hashTable;
+      console.log(groupList);
+      return groupList;
+
     }
 
     beautify(title: string) {
       const day = dayjs(title);
       const now = dayjs();
-      if(day.isSame(now,'day')){
-        return '今天'
-      }else if (day.isSame(now.subtract(1, 'day'), 'day')){
-        return '昨天'
-      }else if(day.isSame(now.subtract(2, 'day'), 'day')){
-        return '前天'
-      }else if(day.isSame(now,'year')){
-        return day.format('M月D日')
-      }else{
-        return day.format('YYYY年M月D日')
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        return '昨天';
+      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+        return '前天';
+      } else if (day.isSame(now, 'year')) {
+        return day.format('M月D日');
+      } else {
+        return day.format('YYYY年M月D日');
       }
-        }
+    }
 
     tagString(tags: Tag[]) {
       const arrName = [];
