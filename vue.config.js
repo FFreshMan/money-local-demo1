07b75d9fe7241a9acdf8ec9663e40bb2f90cf3a9
FFreshman <1366157248@qq.com>
2020-03-05 21:57:35 +0800

    完结撒花！！

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
 const path = require('path')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/money-local-demo1/'
+    ? '/money-local-demonstration/'
     : '/',
   lintOnSave: false,
   chainWebpack: config => {
