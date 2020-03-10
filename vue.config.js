43b6feae319ee170a56b106835a6346581137577
FFreshman <1366157248@qq.com>
2020-03-10 17:37:46 +0800

    â修改路径2

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@
 const path = require('path')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/money-local-demonstration/'
-    : '/',
+      ? '/money-local-demonstration/'
+      : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
